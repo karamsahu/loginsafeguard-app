132643c5935bd8a428a1ba1991516d9877fe36cb
karamchand sahu <capofilainfotech@gmail.com>
2018-07-25 01:02:56 +0530

    added login, dashboard, and regsiter components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+{path: '', redirectTo: 'login', pathMatch: 'full'},
+{path: 'login', component: LoginComponent},
+{path: 'register', component: RegisterComponent},
+{path: 'user', component: DashboardComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
