7e52520117a936d0383cca590caf4a18fca9a9ba
karamsahu <karamsahu@gmail.com>
2018-07-25 11:00:34 +0530

    adding form angular form control and validator for login support

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '../../../node_modules/@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +9,26 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor(private _router: Router) { }
+
+  loginForm: FormGroup = new FormGroup({
+    loginEmail: new FormControl(null, Validators.required),
+    loginPassword: new FormControl(null, Validators.required)
+  });
 
   ngOnInit() {
   }
+  // go to register view
+  navigateToRegister() {
+    this._router.navigate(['/register']);
+  }
 
+  // validate login input
+  login() {
+	//todo : validate from databae and use logger
+    if (this.loginForm.valid && this.loginForm.controls.loginEmail.value === this.loginForm.controls.loginPassword.value) {
+      console.log('logged in succesfully' + JSON.stringify(this.loginForm.value));
+      return true;
+    }
+  }
 }
