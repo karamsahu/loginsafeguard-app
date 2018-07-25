60d25052c157ba9d15b68eaddeeb2066b408fc36
karamsahu <karamsahu@gmail.com>
2018-07-25 09:31:45 +0530

    added code changes related to routes in main landing page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ const routes: Routes = [
 {path: '', redirectTo: 'login', pathMatch: 'full'},
 {path: 'login', component: LoginComponent},
 {path: 'register', component: RegisterComponent},
-{path: 'user', component: DashboardComponent}
+{path: 'dashboard', component: DashboardComponent}
 ];
 
 @NgModule({
