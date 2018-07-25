e4ad1f8f29a93a41d9bd27228556ed8191553289
karamsahu <karamsahu@gmail.com>
2018-07-25 11:31:45 +0530

    added binding and validation for register component

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '../../../node_modules/@angular/router';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
@@ -7,9 +8,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  // create method for form biding
+  registerForm: FormGroup = new FormGroup({
+    signupEmail: new FormControl(null, [Validators.required]),
+    username: new FormControl(null, [Validators.required]),
+    signupPassword: new FormControl(null, [Validators.required]),
+    confirmPassword: new FormControl(null, [Validators.required])
+  });
+
+  constructor(private _routes: Router) { }
 
   ngOnInit() {
   }
 
+  register() {
+    if (!this.registerForm.valid) {
+      console.log('All requried data is not entered.' + JSON.stringify (this.registerForm.value));
+      return false;
+    }
+
+    if (this.registerForm.controls.signupPassword.value !== this.registerForm.controls.confirmPassword.value) {
+      console.log('Username and password must be same.' + JSON.stringify(this.registerForm.value));
+      return false;
+    }
+
+  }
+
+  navigateToLogin() {
+    this._routes.navigate(['/login']);
+  }
 }
